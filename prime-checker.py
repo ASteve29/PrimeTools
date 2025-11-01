744f18ed57ccf7ed7575da29a101d91bd4357548
ASteve29 <29hyperloop@gmail.com>
2025-11-01 18:49:38 +0000

    commit

--- prime-checker.py
+++ prime-checker.py
@@ -1,9 +1,11 @@
 import math
 
 def prime_checker(n):
-  for i in range(2, math.isqrt(n)):
+  if n < 2:
+    return False
+  for i in range(2, math.isqrt(n) + 1):
     if n % i == 0:
-      return bool(0)
+      return False
     else:
       continue
-  return bool(1)
+  return True
